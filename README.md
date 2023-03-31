17980191b0fa0acf66c42139a55dd43fa7be69a4
Joaquim da Rosa <joaquimacrosa@outlook.com>
2023-03-31 17:07:35 +0100

    feat(plugin): updates readme and adds function to .plugin.zsh file

--- README.md
+++ README.md
@@ -1,1 +1,25 @@
 # nvm-checker
+
+=============
+
+**nvm version checker for [Zsh](https://www.zsh.org/).**
+
+_This projects aims at providing users a visual aid to notice that their node version is not equal to the one required on the .nvmrc file_
+
+=============
+
+## Install
+
+### Using [oh-my-zsh](https://github.com/ohmyzsh/ohmyzsh)
+
+1.  Clone this repository in oh-my-zsh's plugins directory:
+
+        git clone https://github.com/significa/nvm-checker.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/nvm-checker
+
+2.  Activate the plugin in `~/.zshrc`:
+
+        plugins=( [plugins...] nvm-checker)
+
+3.  Source `~/.zshrc` to take changes into account:
+
+        source ~/.zshrc
---
+++ nvm-checker.plugin.zsh
@@ -0,0 +1,23 @@
+# Compare if the current node version is the same as the project node version
+function valid_node() {
+    if [[ -f ".nvmrc" ]]; then
+        # Gets nvmrc version
+        local nvmrc_version=$(cat .nvmrc)
+        # Gets current node version
+        local current_node_version=$(nvm version)
+
+        # Gets nvmrc version major number
+        local version_number=${nvmrc_version#?v} 
+
+        local node_version=${current_node_version:1}
+        # Gets current node version major number
+        node_version=${node_version%%.*}
+
+        if [[ ${nvmrc_version:0:2} != ${current_node_version:1:2} ]]; then
+            echo "%{$bg[red]%}%{$fg[230]%} ⚠️  NODE VERSION %{$reset_color%}"
+        fi
+    fi
+}
+
+# Add node version validator to the right side of the prompt
+RPROMPT='$(valid_node)'
