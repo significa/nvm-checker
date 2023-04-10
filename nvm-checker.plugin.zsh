2f209d241d98b995f897c6fe51b4242253e6f18c
Joaquim da Rosa <joaquimacrosa@outlook.com>
2023-04-10 18:38:44 +0100

    fix(quotes): adds quotes to prevent globing

--- nvm-checker.plugin.zsh
+++ nvm-checker.plugin.zsh
@@ -2,21 +2,21 @@
 function valid_node() {
     if [[ -f ".nvmrc" ]]; then
         # Gets nvmrc version
-        local nvmrc_version=$(cat .nvmrc)
+        local nvmrc_version="$(cat .nvmrc)"
         # Gets current node version
-        local current_node_version=$(node -v)
+        local current_node_version="$(node -v)"
         # Gets nvmrc version major number
-        local nvmrc_major_version=$(echo $nvmrc_version | tr -d "v")
-        nvmrc_major_version=${nvmrc_major_version%%.*}
+        local nvmrc_major_version=$(echo "$nvmrc_version" | tr -d "v")
+        nvmrc_major_version="${nvmrc_major_version%%.*}"
         # Gets current node version major number
-        local node_major_version=$(echo $current_node_version | tr -d "v")
-        node_major_version=${node_major_version%%.*}
+        local node_major_version=$(echo "$current_node_version" | tr -d "v")
+        node_major_version="${node_major_version%%.*}"
 
-        if [[ ${nvmrc_major_version} != ${node_major_version} ]]; then
+        if [[ "${nvmrc_major_version}" != "${node_major_version}" ]]; then
             echo "%{$bg[red]%}%{$fg[230]%} ⚠️  NODE VERSION %{$reset_color%}"
         fi
     fi
 }
 
 # Add node version validator to the right side of the prompt
-RPROMPT='$(valid_node)'
+RPROMPT="$(valid_node)"
